d12af67db3362c1c23daa08241e706e500ff1312
Dean Flint <31018066+IAmLeftie@users.noreply.github.com>
2026-07-07 11:49:35 -0400

    Initial commit

---
+++ ScavTemplate/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using MonoMod.RuntimeDetour;
+using UnityEngine;
+
+namespace ModNamespace
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string ModGUID = "modauthor.modname";
+        public const string ModName = "ModName";
+        public const string ModVersion = "0.0.0";
+
+        internal static new ManualLogSource Logger;
+        private readonly Harmony _harmony = new(ModGUID);
+        public static Plugin Instance { get; private set; } = null!;
+
+        void Awake()
+        {
+            Logger = base.Logger;
+            Instance = this;
+
+            _harmony.PatchAll();
+            Logger.LogInfo($"Plugin {ModName} is loaded!");
+        }
+
+        void OnDestroy()
+        {
+            _harmony?.UnpatchSelf();
+            Instance = null!;
+        }
+    }
+}
